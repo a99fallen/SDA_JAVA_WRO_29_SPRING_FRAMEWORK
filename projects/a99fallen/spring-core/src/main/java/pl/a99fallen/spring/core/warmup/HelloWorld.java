f46fd97f2dcf6735922198926a44a81f3b407e46
fallen_a99 <fallen_a99@o2.pl>
2020-10-18 13:18:19 +0200

    dodanie złożonego randomu dla losowania printera bez if/else

--- projects/a99fallen/spring-core/src/main/java/pl/a99fallen/spring/core/warmup/HelloWorld.java
+++ projects/a99fallen/spring-core/src/main/java/pl/a99fallen/spring/core/warmup/HelloWorld.java
@@ -8,6 +8,8 @@ import pl.a99fallen.spring.core.components.printers.ConsolePrinter;
 import pl.a99fallen.spring.core.components.printers.DialogPrinter;
 import pl.a99fallen.spring.core.components.printers.Printer;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
 import java.util.Random;
 
 @Component
@@ -37,11 +39,23 @@ public class HelloWorld {
     }
 
     public void sayHello() {
-        if (new Random().nextInt(2) > 0) {
-            dialogPrinter.print("Hello, world!");
-        }
-        else {
-            consolePrinter.print("Hello, world!");
-        }
+        random();
+//        if (new Random().nextInt(2) > 0) {
+//            dialogPrinter.print("Hello, world!");
+//        }
+//        else {
+//            consolePrinter.print("Hello, world!");
+//        }
+    }
+
+    public void random() {
+        String helloMsg = "Hello World!";
+        Integer r = new Random().nextInt(3);
+        Optional.of(r).filter(v -> v.equals(0))
+                .ifPresentOrElse(v -> consolePrinter.print(helloMsg),
+                        () -> Optional.of(r).filter(v -> v.equals(1))
+                                .ifPresentOrElse(
+                                        v -> dialogPrinter.print(helloMsg),
+                        () -> somePrinter.print(helloMsg)));
     }
 }

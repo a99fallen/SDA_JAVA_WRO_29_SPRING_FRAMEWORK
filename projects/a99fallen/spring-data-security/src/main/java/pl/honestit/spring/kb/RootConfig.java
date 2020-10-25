867a278f4cde9c3b51d95d5c55b746c875de305f
fallen_a99 <fallen_a99@o2.pl>
2020-10-25 14:17:11 +0100

    poprawiony kod podstawy modelu C

--- projects/a99fallen/spring-data-security/src/main/java/pl/honestit/spring/kb/RootConfig.java
+++ projects/a99fallen/spring-data-security/src/main/java/pl/honestit/spring/kb/RootConfig.java
@@ -3,7 +3,7 @@ package pl.honestit.spring.kb;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "pl.honestit.spring.kb.data.repository")
 @EnableTransactionManagement
-@EnableWebSecurity
+
 public class RootConfig {
 
 }
